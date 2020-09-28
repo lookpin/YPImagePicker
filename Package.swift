cbc7837d449b59abc7b7e088eb3acca04dc3a041
saegyeol <yoonsaegyeol@gmail.com>
2020-09-28 17:18:09 +0900

    edit package

--- Package.swift
+++ Package.swift
@@ -5,26 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "YPImagePicker",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "YPImagePicker",
-            targets: ["YPImagePicker"]),
-    ],
+    defaultLocalization: "en",
+    platforms: [.iOS(.v12)],
+    products: [ .library(name: "YPImagePicker", targets: ["YPImagePicker"]) ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/HHK1/PryntTrimmerView.git", .upToNextMajor(from: "4.0.2")),
-      .package(url: "https://github.com/freshOS/Stevia.git", .upToNextMajor(from: "5.1.0"))
-
-      
+        .package(url: "https://github.com/freshOS/Stevia", .exact("5.1.0")),
+        .package(url: "https://github.com/HHK1/PryntTrimmerView", .exact("4.0.2"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "YPImagePicker",
-            dependencies: [],
-            path: "Source")
+        .target(name: "YPImagePicker", dependencies: ["Stevia", "PryntTrimmerView"]),
+        .testTarget(name: "YPImagePickerTests", dependencies: ["YPImagePicker"]),
     ]
 )
